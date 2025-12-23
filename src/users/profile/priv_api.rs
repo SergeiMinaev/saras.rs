1dcec354814566cc5c2135d03b04868cb93fd2ad
Sergei Minaev <brainsburn@yandex.ru>
2025-12-23 10:58:05 +0300

    set_avatar

--- src/users/profile/priv_api.rs
+++ src/users/profile/priv_api.rs
@@ -4,12 +4,21 @@ use crate::http::JsonResp;
 use crate::errors::Error;
 use crate::users::profile::service;
 use crate::users::profile::forms::SetNameForm;
+use crate::forms::image_field_form::ImageFieldForm;
+use crate::users::avatars::service::{update_avatar, delete_avatar};
 use crate::request::RequestTools;
 
 
 pub async fn name(req: Request) -> Resp {
     match req.method.as_str() {
       "put" => return set_name(req).await,
+      _ => return http::not_found()
+    }
+}
+
+pub async fn avatar(req: Request) -> Resp {
+    match req.method.as_str() {
+      "put" => return set_avatar(req).await,
       _ => return http::not_found()
     }
 }
@@ -23,6 +32,28 @@ pub async fn set_name(req: Request) -> Resp {
 			match service::set_name(user.id, &form.name).await {
 				false => JsonResp::err("Не удалось сохранить имя.", &Error::Database).to_http(),
 				true => JsonResp::ok("Имя сохранено.").to_http(),
+			}
+		},
+	}
+}
+
+pub async fn set_avatar(req: Request) -> Resp {
+	let user = req.get_user().await.unwrap();
+	match ImageFieldForm::validate(&req) {
+		Err(_e) => JsonResp::err("Не удалось сохранить аватар.", &Error::Validation).to_http(),
+		Ok(form) => {
+			if form.del.unwrap_or(false) {
+				match delete_avatar(user.id.try_into().unwrap()).await {
+					Ok(()) => JsonResp::ok("Аватар удалён.").to_http(),
+					Err(e) => JsonResp::err("Не удалось удалить аватар.", &e).to_http(),
+				}
+			} else if form.data_base64.is_none() {
+				JsonResp::err("Не удалось сохранить аватар.", &Error::Validation).to_http()
+			} else {
+				match update_avatar(user.id.try_into().unwrap(), &form).await {
+					Ok(()) => JsonResp::ok("Аватар сохранён.").to_http(),
+					Err(e) => JsonResp::err("Не удалось сохранить аватар.", &e).to_http(),
+				}
 			}
 		},
 	}
