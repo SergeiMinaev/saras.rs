644f85918addf798a58035918a48a465c8287ad0
Sergei Minaev <brainsburn@yandex.ru>
2026-01-29 22:19:02 +0300

    Дефолтный аватар генерится при установке name

--- src/users/profile/service.rs
+++ src/users/profile/service.rs
@@ -1,10 +1,18 @@
 use crate::db::get_pool;
 use crate::users::profile::db::ProfileDb;
+use crate::users::avatars::service::update_default_avatar;
 
 
 
 pub async fn set_name(user_id: u32, name: &str) -> bool {
 	let pool = get_pool();
 	let profiledb = ProfileDb::new(pool);
-	profiledb.set_name(user_id, name).await
+	let ok = profiledb.set_name(user_id, name).await;
+	if ok {
+		let user_id_i32: i32 = user_id.try_into().unwrap_or_default();
+		if user_id_i32 > 0 {
+			let _ = update_default_avatar(user_id_i32).await;
+		}
+	}
+	ok
 }
